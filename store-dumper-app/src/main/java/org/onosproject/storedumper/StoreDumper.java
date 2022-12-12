5da376c1dbc351f0c243c884fd910150d40ff293
edoardottt <edoardott@gmail.com>
2022-12-12 18:38:05 +0100

    Working example

--- store-dumper-app/src/main/java/org/onosproject/storedumper/StoreDumper.java
+++ store-dumper-app/src/main/java/org/onosproject/storedumper/StoreDumper.java
@@ -18,10 +18,41 @@ package org.onosproject.storedumper;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.onosproject.core.CoreService;
+import org.onosproject.net.Host;
+import org.onosproject.net.host.HostService;
 import org.onosproject.net.host.HostStore;
+import org.onosproject.net.device.DeviceService;
 import org.onosproject.net.device.DeviceStore;
+import org.onosproject.net.Device;
 import org.onosproject.net.packet.PacketStore;
+import org.onosproject.net.statistic.StatisticStore;
+import org.onosproject.net.resource.ResourceStore;
+import org.onosproject.net.region.RegionStore;
+import org.onosproject.net.meter.MeterStore;
+import org.onosproject.net.link.LinkStore;
+import org.onosproject.net.key.DeviceKeyStore;
+import org.onosproject.net.intent.IntentStore;
+import org.onosproject.net.group.GroupStore;
+import org.onosproject.net.flowobjective.FlowObjectiveStore;
+import org.onosproject.net.flow.FlowRuleStore;
+import org.onosproject.net.config.NetworkConfigStore;
+import org.onosproject.cluster.ClusterStore;
+import org.onosproject.app.ApplicationIdStore;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.io.File;
+import java.nio.file.Files;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,11 +69,22 @@ public class StoreDumper {
     // --------------------------------------------------------
     private static final long TIMEOUT = 60000;
 
+    // --------------------------------------------------------
+    // CHANGE THIS PARAMETER TO WRITE IN A DIFFERENT FILE.
+    // --------------------------------------------------------
+    private static final String FILENAME = "/home/edoardottt/cybersecurity/todo/Thesis/onos-dump.txt";
+
     @Reference(cardinality = ReferenceCardinality.MANDATORY)
     protected CoreService coreService;
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected HostService hostService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
     protected HostStore hostStore;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected DeviceService deviceService;
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY)
     protected DeviceStore deviceStore;
@@ -101,11 +143,14 @@ public class StoreDumper {
     @Activate
     protected void activate() {
         coreService.registerApplication("org.edoardottt.storedumper.app", () -> log.info("Periscope down."));
+        startTimer(TIMEOUT);
         log.info("Started storedumper App!");
     }
 
     @Deactivate
     protected void deactivate() {
+        timer.cancel();
+        timer.purge();
         log.info("Stopped storedumper App!");
     }
 
@@ -115,17 +160,58 @@ public class StoreDumper {
     }
 
     private void dump() {
-        getHosts();
+        createFile();
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
+        writeToFile("=========================== " + timeStamp + " ===========================");
+        dumpHosts();
+        dumpDevices();
     }
 
-    // getHosts
-    private void getHosts() {
+    // dumpHosts
+    private void dumpHosts() {
+        writeToFile("------------ HOSTS DUMP ------------");
         Iterable<Host> hosts = hostService.getHosts();
-        Random rand = new Random();
         List<Host> hostList = new ArrayList<Host>();
         hosts.forEach(hostList::add);
         for (int i=0; i<hostList.size(); i++) {
-            log.info(hostList.get(i).toString());
+            writeToFile(hostList.get(i).toString());
+        }
+    }
+
+    // dumpDevices
+    private void dumpDevices() {
+        writeToFile("------------ DEVICES DUMP ------------");
+        Iterable<Device> devices = deviceService.getDevices();
+        List<Device> deviceList = new ArrayList<Device>();
+        devices.forEach(deviceList::add);
+        for (int i=0; i<deviceList.size(); i++) {
+            writeToFile(deviceList.get(i).toString());
+        }
+    }
+
+    private File createFile() {
+        try {
+            File myObj = new File(FILENAME);
+            return myObj;
+        } catch (Exception e) {
+            log.info("Error while creating file {}", FILENAME);
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void writeToFile(String content) {
+        try {
+            FileWriter fw = new FileWriter(FILENAME, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw);
+            out.println(content + "\n");
+            out.close();
+            log.info("Successfully wrote to the file!");
+        } catch (IOException e) {
+            log.info("Error while writing to file {}", FILENAME);
+            e.printStackTrace();
         }
     }
 }
