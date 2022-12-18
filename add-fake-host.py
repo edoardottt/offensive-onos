b6a5de9c6b487ad70089ab2c5ee49950f6517479
vrenzolaverace <edoardott@gmail.com>
2022-12-18 09:45:54 +0100

    Update add-fake-host.py

--- add-fake-host.py
+++ add-fake-host.py
@@ -1,3 +1,7 @@
+# Send this IP packet with `python add-fake-host.py`.
+# If Host Location Provider is activated a new host with IP 10.0.0.5 will be added
+# to the host store.
+
 from scapy.all import *
 
 frame = Ether(src="00:00:00:00:00:05", dst="00:00:00:00:00:01")/IP(src="10.0.0.5", dst="10.0.0.4")
