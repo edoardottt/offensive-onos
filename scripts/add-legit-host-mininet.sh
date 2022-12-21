e8248d5c4c004ba61379eec9b4c683ac0020a384
vrenzolaverace <edoardott@gmail.com>
2022-12-21 18:48:12 +0100

    Update add-legit-host-mininet.sh

--- scripts/add-legit-host-mininet.sh
+++ scripts/add-legit-host-mininet.sh
@@ -1,1 +1,6 @@
 # Add a host in mininet while the network is up nd running.
+
+py net.addHost('h5')
+py net.addLink(s4, net.get('h5'))
+py s4.attach('s4-eth3')
+py net.get('h5').cmd('ifconfig h5-eth0 10.0.0.5')
