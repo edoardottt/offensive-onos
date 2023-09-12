44fe6ea5e591cfc79ff39423980de078ec291ccf
edoardottt <edoardott@gmail.com>
2023-09-12 11:10:34 +0200

    update dhcp ipsat

--- apps/dhcpipsat-app/src/main/java/org/dhcpipsat/app/DhcpIpSat.java
+++ apps/dhcpipsat-app/src/main/java/org/dhcpipsat/app/DhcpIpSat.java
@@ -16,10 +16,7 @@
  * @author edoardottt, https://www.edoardoottavianelli.it/
  */
 
-package org.dhcpipsat.app;
-/*
- * package org.onosproject.dhcpipsat;
- */
+package org.onosproject.dhcpipsat;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -77,6 +74,9 @@ import org.onosproject.net.host.HostProvider;
 import org.onosproject.net.host.HostProviderRegistry;
 import org.onosproject.net.host.HostProviderService;
 import org.onosproject.net.packet.DefaultOutboundPacket;
+import org.onosproject.net.host.HostEvent;
+import org.onosproject.net.host.HostListener;
+import java.util.concurrent.ExecutorService;
 import org.onosproject.net.packet.PacketContext;
 import org.onosproject.net.packet.PacketPriority;
 import org.onosproject.net.packet.PacketProcessor;
@@ -93,6 +93,9 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
+import static org.onlab.util.Tools.groupedThreads;
+
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Date;
@@ -102,6 +105,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -139,87 +143,119 @@ public class DhcpIpSat {
     @Reference(cardinality = ReferenceCardinality.MANDATORY)
     protected IpAddressPoolStore ipAddressPoolStore;
 
-    private DhcpPacketProcessor processor = new DhcpPacketProcessor();
+    protected ExecutorService eventExecutor;
+
+    private final InternalHostListener hostListener = new InternalHostListener();
+
+    // private DhcpPacketProcessor processor = new DhcpPacketProcessor();
 
     private ApplicationId appId;
 
     @Activate
     protected void activate() {
         appId = coreService.registerApplication("org.onosproject.dhcpipsat.app", () -> log.info("Periscope down."));
-        packetService.addProcessor(processor, PacketProcessor.director(1));
-        requestPackets();
+        // packetService.addProcessor(processor, PacketProcessor.director(1));
+        // requestPackets();
+        eventExecutor = newSingleThreadScheduledExecutor(groupedThreads("onos/dhcpipsat", "events-%d", log));
+        hostService.addListener(hostListener);
         log.info("Started dhcpipsat App!");
     }
 
     @Deactivate
     protected void deactivate() {
-        packetService.removeProcessor(processor);
-        cancelPackets();
+        // packetService.removeProcessor(processor);
+        // cancelPackets();
+        hostService.removeListener(hostListener);
+        eventExecutor.shutdownNow();
+        eventExecutor = null;
         log.info("Stopped dhcpipsat App!");
     }
 
-    /**
-     * Request packet in via PacketService.
-     */
-    private void requestPackets() {
-
-        TrafficSelector.Builder selectorServer = DefaultTrafficSelector.builder()
-                .matchEthType(Ethernet.TYPE_IPV4)
-                .matchIPProtocol(IPv4.PROTOCOL_UDP)
-                .matchUdpDst(TpPort.tpPort(UDP.DHCP_SERVER_PORT))
-                .matchUdpSrc(TpPort.tpPort(UDP.DHCP_CLIENT_PORT));
-        packetService.requestPackets(selectorServer.build(), PacketPriority.CONTROL, appId);
-
-        selectorServer = DefaultTrafficSelector.builder()
-                .matchEthType(Ethernet.TYPE_ARP);
-        packetService.requestPackets(selectorServer.build(), PacketPriority.CONTROL, appId);
-    }
-
-    /**
-     * Cancel requested packets in via packet service.
-     */
-    private void cancelPackets() {
-        TrafficSelector.Builder selectorServer = DefaultTrafficSelector.builder()
-                .matchEthType(Ethernet.TYPE_IPV4)
-                .matchIPProtocol(IPv4.PROTOCOL_UDP)
-                .matchUdpDst(TpPort.tpPort(UDP.DHCP_SERVER_PORT))
-                .matchUdpSrc(TpPort.tpPort(UDP.DHCP_CLIENT_PORT));
-        packetService.cancelPackets(selectorServer.build(), PacketPriority.CONTROL, appId);
-
-        selectorServer = DefaultTrafficSelector.builder()
-                .matchEthType(Ethernet.TYPE_ARP);
-        packetService.cancelPackets(selectorServer.build(), PacketPriority.CONTROL, appId);
-    }
-
-    private class DhcpPacketProcessor implements PacketProcessor {
-
+    private class InternalHostListener implements HostListener {
         @Override
-        public void process(PacketContext context) {
-            Ethernet packet = context.inPacket().parsed();
-            if (packet.getEtherType() == Ethernet.TYPE_IPV4) {
-                IPv4 ipv4Packet = (IPv4) packet.getPayload();
-
-                if (ipv4Packet.getProtocol() == IPv4.PROTOCOL_UDP) {
-                    UDP udpPacket = (UDP) ipv4Packet.getPayload();
-
-                    if (udpPacket.getDestinationPort() == UDP.DHCP_SERVER_PORT ||
-                            udpPacket.getSourcePort() == UDP.DHCP_CLIENT_PORT) {
-                        // This is meant for the dhcp server so process the packet here.
-                        log.info("received DHCP packet!");
-
-                        // read from host data store which IP addresses have been taken
-                        ArrayList<String> ips = getIpAddresses();
-
-                        // update the IP address pool store
-                        for (String ip : ips) {
-                            int lastDigits = Integer.valueOf(ip.split("\\.")[3]);
-                            ipAddressPoolStore.ipTaken(lastDigits);
-                        }
-                    }
+        public void event(HostEvent event) {
+            eventExecutor.execute(() -> {
+                // read from host data store which IP addresses have been taken
+                // ArrayList<String> ips = getIpAddresses();
+                log.info("received EVENT!");
+                Host h = event.subject();
+                ArrayList<String> ipList = new ArrayList<String>();
+                for (IpAddress ipa : h.ipAddresses()) {
+                    ipList.add(ipa.toString());
                 }
-            }
+
+                // update the IP address pool store
+                for (String ip : ipList) {
+                    int lastDigits = Integer.valueOf(ip.split("\\.")[3]);
+                    ipAddressPoolStore.ipTaken(lastDigits);
+                }
+            });
         }
+
     }
+
+    /*
+     * private void requestPackets() {
+     * 
+     * TrafficSelector.Builder selectorServer = DefaultTrafficSelector.builder()
+     * .matchEthType(Ethernet.TYPE_IPV4)
+     * .matchIPProtocol(IPv4.PROTOCOL_UDP)
+     * .matchUdpDst(TpPort.tpPort(UDP.DHCP_SERVER_PORT))
+     * .matchUdpSrc(TpPort.tpPort(UDP.DHCP_CLIENT_PORT));
+     * packetService.requestPackets(selectorServer.build(), PacketPriority.CONTROL,
+     * appId);
+     * 
+     * selectorServer = DefaultTrafficSelector.builder()
+     * .matchEthType(Ethernet.TYPE_ARP);
+     * packetService.requestPackets(selectorServer.build(), PacketPriority.CONTROL,
+     * appId);
+     * }
+     * 
+     * private void cancelPackets() {
+     * TrafficSelector.Builder selectorServer = DefaultTrafficSelector.builder()
+     * .matchEthType(Ethernet.TYPE_IPV4)
+     * .matchIPProtocol(IPv4.PROTOCOL_UDP)
+     * .matchUdpDst(TpPort.tpPort(UDP.DHCP_SERVER_PORT))
+     * .matchUdpSrc(TpPort.tpPort(UDP.DHCP_CLIENT_PORT));
+     * packetService.cancelPackets(selectorServer.build(), PacketPriority.CONTROL,
+     * appId);
+     * 
+     * selectorServer = DefaultTrafficSelector.builder()
+     * .matchEthType(Ethernet.TYPE_ARP);
+     * packetService.cancelPackets(selectorServer.build(), PacketPriority.CONTROL,
+     * appId);
+     * }
+     * 
+     * private class DhcpPacketProcessor implements PacketProcessor {
+     * 
+     * @Override
+     * public void process(PacketContext context) {
+     * Ethernet packet = context.inPacket().parsed();
+     * if (packet.getEtherType() == Ethernet.TYPE_IPV4) {
+     * IPv4 ipv4Packet = (IPv4) packet.getPayload();
+     * 
+     * if (ipv4Packet.getProtocol() == IPv4.PROTOCOL_UDP) {
+     * UDP udpPacket = (UDP) ipv4Packet.getPayload();
+     * 
+     * if (udpPacket.getDestinationPort() == UDP.DHCP_SERVER_PORT ||
+     * udpPacket.getSourcePort() == UDP.DHCP_CLIENT_PORT) {
+     * // This is meant for the dhcp server so process the packet here.
+     * log.info("received DHCP packet!");
+     * 
+     * // read from host data store which IP addresses have been taken
+     * ArrayList<String> ips = getIpAddresses();
+     * 
+     * // update the IP address pool store
+     * for (String ip : ips) {
+     * int lastDigits = Integer.valueOf(ip.split("\\.")[3]);
+     * ipAddressPoolStore.ipTaken(lastDigits);
+     * }
+     * }
+     * }
+     * }
+     * }
+     * }
+     */
 
     // getHosts
     private ArrayList<String> getIpAddresses() {
